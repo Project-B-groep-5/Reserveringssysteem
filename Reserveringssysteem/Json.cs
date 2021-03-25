08eb52652df4dabaf2510747cf7bad8df0fdde8d
Ovvugt <oscar.vugt@gmail.com>
2021-03-25 20:52:39 +0100

    Refactored Program.cs

---
+++ Reserveringssysteem/Json.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace Reserveringssysteem
+{
+    public static class Json
+    {
+        public static void JsonSerialize(object[] obj, string filename)
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            var jsonString = JsonSerializer.Serialize(obj, options);
+            File.WriteAllText(filename, jsonString);
+        }
+    }
+}
---
+++ Reserveringssysteem/SelectionMenu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reserveringssysteem
+{
+    class SelectionMenu
+    {
+        private string[] menuArray;
+
+        public SelectionMenu(string[] array)
+        {
+            menuArray = array;
+        }
+
+        public void Show()
+        {
+            var optionsCount = menuArray.Length;
+            var optionSelected = 0;
+            var done = false;
+            var menuArrow = "-> ";
+            var logo = @"
+ _____           _           _     ____  
+|  __ \         (_)         | |   |  _ \ 
+| |__) | __ ___  _  ___  ___| |_  | |_) |
+|  ___/ '__/ _ \| |/ _ \/ __| __| |  _ < 
+| |   | | | (_) | |  __/ (__| |_  | |_) |
+|_|   |_|  \___/| |\___|\___|\__| |____/ 
+               _/ |                      
+              |__/   Console v0.1";
+
+            Console.WriteLine(logo + "\n");
+            while (!done)
+            {
+                for (int i = 0; i < optionsCount; i++)
+                {
+                    if (optionSelected == i)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.Write(menuArrow);
+                    }
+                    else
+                    {
+                        Console.Write("   "); //length of the menuArrow in spaces
+                    }
+                    Console.WriteLine(menuArray[i]);
+                    Console.ResetColor();
+                }
+
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        optionSelected = Math.Max(0, optionSelected - 1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        optionSelected = Math.Min(optionsCount - 1, optionSelected + 1);
+                        break;
+                    case ConsoleKey.Enter:
+                        done = true;
+                        break;
+                }
+                if (!done)
+                {
+                    Console.CursorTop = Console.CursorTop - optionsCount;
+                }
+            }
+        }
+    }
+}
