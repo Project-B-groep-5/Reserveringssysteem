3fd7a7a31d8db3a4c0e95833386c253d77b9a5c1
Ovvugt <oscar.vugt@gmail.com>
2021-03-26 18:24:13 +0100

    Added Json Deserialization for tables.json and coverted Tafels class to the english counterpart Tables

--- Reserveringssysteem/Program.cs
+++ Reserveringssysteem/Program.cs
@@ -7,9 +7,8 @@ namespace Reserveringssysteem
     {
         static void Main(string[] args)
         {
-            JsonSerialize(new Tafel[] {new Tafel(1, 6, new Klant("Oscar"), new Rekening(0.00)), new Tafel(2, 4, new Klant("Arjen"), new Rekening(100.0)) }, "tafels.json");
+            Serialize(new Table[] {new Table("1", 6, new Customer("Oscar"), new Bill(0.00)), new Table("2", 4, new Customer("Arjen"), new Bill(100.0)) }, "tables.json");
             var introMenu = new SelectionMenu(new string[4] { "1] Plaats een reservering", "2] Bekijk het menukaart", "3] Informatie over ons", "[Voor Medewerkers]" });
-            
             introMenu.Show();
         }
 
