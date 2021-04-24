2dd5424c7702cf1ba5b42ae677479cb5c2a01eab
slowflowanimal01 <0990651@hr.nl>
2021-04-24 17:57:32 +0200

    Infoscherm beetje ingevuld

--- Reserveringssysteem/InfoScherm.cs
+++ Reserveringssysteem/InfoScherm.cs
@@ -8,7 +8,15 @@ namespace Reserveringssysteem
     {
         public static void ShowInfo()
         {
-            Console.WriteLine(Logo.OverOns + "\nOver ons");
+            Console.WriteLine(Logo.OverOns);
+            Console.WriteLine("Restaurant informatie:\n" +
+                "Welkom bij de Houten Vork. Wij bezorgen u een lach met de lekkerste gerechten.\n" +
+                "Wij bereiden de lekkerste vlees en vegetarische gerechten die u terug kunt vinden " +
+                "in het menukaart.\n\n" +
+                "Restaurant gegevens:\n"+
+                "Adress: ........\n" +
+                "Telefoonnummer: ........\n" +
+                "E-mail: ........ ");
         }
     }
 }
