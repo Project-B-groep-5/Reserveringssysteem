07a122da258c50c081de16f73509860cefd92bf0
Jeffrey <75099539+JeffreyHub@users.noreply.github.com>
2021-04-24 17:45:47 +0200

    de class reservationcheck gemaakt

---
+++ Reserveringssysteem/ReservationCheck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using static Reserveringssysteem.Json;
+
+namespace Reserveringssysteem
+{
+    class ReservationCheck
+    {
+        
+        public static List<Reservation> ReservationList;
+        
+        public static bool Check(string date, string time, int size)
+        {
+            ReservationList = Deserialize<List<Reservation>>("reservations.json");
+            int capacity = 100;
+            int occupied = 0;
+            occupied += size;
+            for (int i = 0; i < ReservationList.Count; i++)
+            {
+                if (ReservationList[i].Date == date && ReservationList[i].Time == time) 
+                    occupied += ReservationList[i].Size;
+            }
+            if (occupied <= capacity) return true;
+            else return false;
+        }
+    }
+}
